efc3039034698cef0e36787543519c319bf7ac8f
Josh Webb <joshwebb42@gmail.com>
2019-09-09 14:58:47 -0500

    crud on ingredients and categories

--- Pantry/Pantry/Models/Ingredient.cs
+++ Pantry/Pantry/Models/Ingredient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace Pantry.Models
 {
     public class Ingredient
     {
+        [Key]
+        public int IngredientId
+        { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9 ]*$")]
+        public string Title { get; set; }
+        [Required]
+        public string CategoryId { get; set; }
+        [Required]
+        public string Quantity { get; set; }
+        [Required]
+        public int UserId { get; set; }
+
     }
 }
--- Pantry/Pantry/Models/Recipe.cs
+++ Pantry/Pantry/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace Pantry.Models
 {
     public class Recipe
     {
+        [Key]
+        public int RecipeId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string ServingSize { get; set; }
+        public string CookTime { get; set; }
+        public int SpoonacularId { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public int UserId { get; set;  }
     }
 }
