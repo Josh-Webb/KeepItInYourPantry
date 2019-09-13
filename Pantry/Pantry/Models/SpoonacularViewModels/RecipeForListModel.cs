e349be339898c5e362d8a9a14e0a7f717c9d4c8f
Josh Webb <joshwebb42@gmail.com>
2019-09-13 15:25:04 -0500

    ticket 5 baby

---
+++ Pantry/Pantry/Models/SpoonacularViewModels/RecipeForListModel.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pantry.Models.SpoonacularViewModels
+{
+    public class RecipeForListModel
+    {
+        public int Id { get; set; }
+        public int SpoonacularId { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string ImageType { get; set; }
+        public int UsedIngredientCount { get; set; }
+        public int MissedIngredientCount { get; set; }
+
+    }
+}
